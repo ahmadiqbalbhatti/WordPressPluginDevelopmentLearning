8c45201806f1801002cc53e17ff44506df8c1334
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2023-02-06 15:01:29 +0500

    New Plugin Initialized

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: Udemy Plus
+Plugin URI: http://ahmadiqbalbhatti.com
+Description: A new plugin created for learning and practice
+Version: 1.0.0
+Author: Ahmad Iqbal Bhatti
+Author URI: http://ahmadiqbalbhatti.com
+*/
