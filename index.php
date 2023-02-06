abb7cbbcb3a71738970ceccfcceab9cda9592af8
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2023-02-06 15:17:44 +0500

    WordPress Scrips add for Gutenberg Blocks

--- index.php
+++ index.php
@@ -8,3 +8,8 @@ Version: 1.0.0
 Author: Ahmad Iqbal Bhatti
 Author URI: http://ahmadiqbalbhatti.com
 */
+
+if (!function_exists('add_action')){
+    echo 'I think you are stumbled here by accident';
+    exit;
+}
